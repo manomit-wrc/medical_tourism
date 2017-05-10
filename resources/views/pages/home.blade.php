30f44340cc8f72c7b79bb15bae7426f2325cbe22
amritawrc <amrita@wrctechnologies.com>
2017-05-10 10:31:25 +0530

    homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -9,8 +9,7 @@
                   <div class="row">
                       <div class="col-md-12">
                           <h2>Medical <strong>category</strong></h2>
-                          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
-industry's standard dummy text</p>
+                          <p>{{ $homepagecondata[0]->medical_category_description }}</p>
                   
                           <div class="jcarousel-wrapper">
                             <div class="jcarousel">
@@ -79,25 +78,25 @@ industry's standard dummy text</p>
 
             <div class="col-md-4">
                 <div class="accdbox">
-                    <a href="{!!URL::to('/connectivity')!!}"><img src="{!!URL::to('storage/frontend/images/acc1.jpg')!!}"  alt="Image 1"></a>
-                    <h3>Connectivity to India</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <a href="{!!URL::to('/connectivity')!!}"><img src="{{url('/uploads/homepagecontent/thumb_360_167/'.$homepagecondata[0]->accomodation_left_image)}}"  alt="Image 1"></a>
+                    <h3>{{ $homepagecondata[0]->accomodation_left_title }}</h3>
+                    <p>{{ $homepagecondata[0]->accomodation_left_description }}</p>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="accdbox">
-                    <a href="{!!URL::to('/immigration')!!}"><img src="{!!URL::to('storage/frontend/images/acc2.jpg')!!}"  alt="Image 1"></a>
-                    <h3>Immigration</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <a href="{!!URL::to('/immigration')!!}"><img src="{{url('/uploads/homepagecontent/thumb_360_167/'.$homepagecondata[0]->accomodation_middle_image)}}"  alt="Image 1"></a>
+                    <h3>{{ $homepagecondata[0]->accomodation_middle_title }}</h3>
+                    <p>{{ $homepagecondata[0]->accomodation_middle_description }}</p>
                 </div>
             </div>
 
             <div class="col-md-4">
                 <div class="accdbox">
-                    <a href="{!!URL::to('/visa')!!}"><img src="{!!URL::to('storage/frontend/images/acc3.jpg')!!}"  alt="Image 1"></a>
-                    <h3>Visa</h3>
-                    <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+                    <a href="{!!URL::to('/visa')!!}"><img src="{{url('/uploads/homepagecontent/thumb_360_167/'.$homepagecondata[0]->accomodation_right_image)}}"  alt="Image 1"></a>
+                    <h3>{{ $homepagecondata[0]->accomodation_right_title }}</h3>
+                    <p>{{ $homepagecondata[0]->accomodation_right_description }}</p>
                 </div>
             </div>
             <br clear="all">
