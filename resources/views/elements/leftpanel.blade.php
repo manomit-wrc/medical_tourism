64a5b9753269d886ccf8f2a08f9206cd33824afc
suman <suman@wrctechnologies.com>
2017-05-30 11:34:33 +0530

    update

--- resources/views/elements/leftpanel.blade.php
+++ resources/views/elements/leftpanel.blade.php
@@ -2,29 +2,49 @@
     <div  id="sticker">
         <div class="qtbox">
             <h3><img src="{!!URL::to('storage/frontend/images/ql.jpg')!!}" alt=""> REQUEST A <strong>QUOTE</strong> <img src="{!!URL::to('storage/frontend/images/qr.jpg')!!}" alt=""></h3>
-
-            <select name="" class="listtypeleft">
-                <option value="Specility">Specility</option>
-                <option value="Specility">Specility</option>
-                <option value="Specility">Specility</option>
+            @php
+              if (app('request')->input('txt_search') !='' && !empty(app('request')->input('txt_search'))){
+                  $txt_search = app('request')->input('txt_search');
+              }else{
+                  $txt_search ='';
+              }
+              if (app('request')->input('select_treatment') !='' && !empty(app('request')->input('select_treatment'))){
+                  $select_treatment = app('request')->input('select_treatment');
+              }else{
+                  $select_treatment ='';
+              }
+              if (app('request')->input('select_procedure') !='' && !empty(app('request')->input('select_procedure'))){
+                  $select_procedure = app('request')->input('select_procedure');
+              }else{
+                  $select_procedure ='';
+              }                  
+              @endphp
+            <form name="frmSearch1" id="frmSearch1" method="get" action="/search-data">
+            {{csrf_field()}}
+            <select name="select_treatment" class="listtypeleft">
+                <option value="">Specility</option>
+                @foreach($treatment_list as $key=>$value)
+                    <option value="{{$key}}" {{ ($select_treatment == $key)? 'selected':'' }}>{{$value}}</option>
+                @endforeach
             </select>
 
-            <select name="" class="listtypeleft">
-                <option value="Procedure">Procedure</option>
-                <option value="Procedure">Procedure</option>
-                <option value="Procedure">Procedure</option>
+            <select name="select_procedure" class="listtypeleft" id="select_procedure">
+                <option value="">Procedure</option>
+                @foreach($procedure_list as $key=>$value)
+                  <option value="{{$key}}" {{ ($select_procedure == $key)? 'selected':'' }}>{{$value}}</option>
+                @endforeach
             </select>
 
-            <select name="" class="listtypeleft">
-                <option value="Location">Location</option>
-                <option value="Location">Location</option>
-                <option value="Location">Location</option>
-            </select>
+           <!--  <select name="" class="listtypeleft">
+               <option value="Location">Location</option>
+               <option value="Location">Location</option>
+               <option value="Location">Location</option>
+           </select> -->
 
-            <input type="text" name="" class="inputleft">  
+            <input type="text" name="txt_search" value="{{ $txt_search }}" id="txt_search" class="inputleft">  
 
-            <button type="button"><i class="fa fa-search" aria-hidden="true"></i> SEARCH</button>
-
+            <button type="submit" id="btn-search"><i class="fa fa-search" aria-hidden="true"></i> SEARCH</button>
+        </form>
         </div>
         @if(Request::segment(1) != 'contact')
             <div class="qc">
