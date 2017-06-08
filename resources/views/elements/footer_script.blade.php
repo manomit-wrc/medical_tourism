0081863a44af6988abc0a4f08ce56433f4103492
manomit-wrc <manomit@wrctechnologies.com>
2017-06-08 12:40:09 +0530

    up

--- resources/views/elements/footer_script.blade.php
+++ resources/views/elements/footer_script.blade.php
@@ -19,9 +19,10 @@
     {!!Html::script("storage/frontend/tag/dist/bootstrap-tagsinput.min.js")!!}
 
     <!-- {!!Html::script("storage/frontend/js/jquery.do.scroll.js")!!}
-    {!!Html::script("storage/frontend/js/script.js")!!} -->
+    {!!Html::script("storage/frontend/js/script.js")!!} 
 
-    {!!Html::script("storage/frontend/js/jquery.sticky.js")!!}
+    {!!Html::script("storage/frontend/js/jquery.sticky.js")!!}-->
+    {!!Html::script("storage/frontend/js/jquery.custom-scrollbar.js")!!}
    
     <script type="text/javascript">
        $(function () {$('[data-toggle="tooltip"]').tooltip()})
