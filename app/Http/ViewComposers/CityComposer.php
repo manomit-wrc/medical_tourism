4272c6ea2ba0635198e7522fce639b1e1714a5ce
amritawrc <amrita@wrctechnologies.com>
2017-06-14 13:44:55 +0530

    uopdate

--- app/Http/ViewComposers/CityComposer.php
+++ app/Http/ViewComposers/CityComposer.php
@@ -6,6 +6,7 @@ use App\City;
 Class CityComposer {
 	public function compose(View $view)
 	{
-      $view->with('city_lists', City::orderByRaw("RAND()")->take(5)->get());
+      //$view->with('city_lists', City::orderByRaw("RAND()")->take(5)->get()); //This will take random data from city
+		$view->with('city_lists', City::whereIn('name', ['Kolkata', 'Chennai','Delhi','Mumbai'])->get());
 	}
 }
--- resources/views/pages/immigration.blade.php
+++ resources/views/pages/immigration.blade.php
@@ -9,6 +9,7 @@
               <table class="table table-bordered tablebg">
               <thead>
                 <tr class="table_topbg">
+                  <th>Country</th>
                   <th>Place</th>
                   <th>Name and Designtion</th>
                   <th>Office address</th>
@@ -24,6 +25,7 @@
                  @if (count($immigration_lists) > 0)
                     @foreach($immigration_lists as $immigration_lists)
                       <tr {{ $i % 2 == 0 ? 'style=background-color:#eeeef0' : '' }} >
+                        <td>{{ $immigration_lists->country->name }}</td>
                         <td>{{ $immigration_lists->city->name }}</td>
                         <td>{{ $immigration_lists->name }} , {{ $immigration_lists->designation }} {{ $immigration_lists->city->name }}</td>
                         <td>{{ $immigration_lists->address }}, {{ $immigration_lists->city->name }}</td>
