5fd34f2e648f017e8cef3e4118b02a0ddb1c33a2
manomit-wrc <manomit@wrctechnologies.com>
2017-07-05 15:02:46 +0530

    Arijit

--- resources/views/pages/my_enquiry_details.blade.php
+++ resources/views/pages/my_enquiry_details.blade.php
@@ -22,8 +22,8 @@
             </div>
 
             <script>
-           function showhide()
-           {
+                 function showhide()
+                 {
                  var div = document.getElementById("newpost");
                     if (div.style.display !== "block") {
                         div.style.display = "block";
@@ -32,6 +32,19 @@
                     else {
                         div.style.display = "none";
                         document.getElementById("abc").style.backgroundColor = "#f2f2f2";
+                    }
+                     }
+
+                     function showhide1()
+                 {
+                 var div = document.getElementById("newpost1");
+                    if (div.style.display !== "block") {
+                        div.style.display = "block";
+                        document.getElementById("abc1").style.backgroundColor = "#fff";
+                    }
+                    else {
+                        div.style.display = "none";
+                        document.getElementById("abc1").style.backgroundColor = "#f2f2f2";
                     }
                      }
           </script>
@@ -48,6 +61,40 @@
                       </div>
                     </a>
                     <div id="newpost" class="maildet" style="display:none;">
+                      <div class="mailsender">
+                      to me
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a>From : arijit@wrctechnologies.com</a></li>
+                          <li><a>to : master@wrctechnologies.com</a></li>
+                        </ul>
+                      </li>
+
+                      </div>
+                      <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                          <li><a href="#"><i class="fa fa-reply" aria-hidden="true"></i> Reply</a></li>
+                        </ul>
+                      </li>
+                      <p>
+                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.
+                      </p>
+                    </div>
+                </div>
+
+                <div id="abc1" class="loopbox">
+                    <a href="javascript:void(0)" onclick="showhide1()">
+                      <div class="row">
+                          <div class="col-xs-1">
+                            <img src="{!!URL::to('storage/frontend/images/master.jpg')!!}"  alt="Image 1">
+                          </div>
+                          <div class="col-xs-9"><div class="loopname">Master Mondal</div></div>
+                          <div class="col-xs-2" align="right"><div class="loopname">7/12/2016</div></div>
+                      </div>
+                    </a>
+                    <div id="newpost1" class="maildet" style="display:none;">
                       <div class="mailsender">
                       to me
                       <li class="dropdown">
