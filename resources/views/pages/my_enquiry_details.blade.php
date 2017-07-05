b064476b7558144e492b2db008e7885bf1b9d58f
amritawrc <amrita@wrctechnologies.com>
2017-07-05 17:12:55 +0530

    update

--- resources/views/pages/my_enquiry_details.blade.php
+++ resources/views/pages/my_enquiry_details.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.inner_layout')
 @section('title', 'Faqs')
 @section('content')
+<script>
+   
+   function showhide(id)
+   {
+      //alert(id);
+      var div = document.getElementById("newpost"+id);
+      if (div.style.display !== "block") {
+          div.style.display = "block";
+          document.getElementById("abc"+id).style.backgroundColor = "#fff";
+      }
+      else {
+          div.style.display = "none";
+          document.getElementById("abc"+id).style.backgroundColor = "#f2f2f2";
+      }
+    }
+
+   /* function showhide1()
+   {
+      var div = document.getElementById("newpost1");
+      if(div.style.display !== "block"){
+          div.style.display = "block";
+          document.getElementById("abc1").style.backgroundColor = "#fff";
+      }else{
+          div.style.display = "none";
+          document.getElementById("abc1").style.backgroundColor = "#f2f2f2";
+      }
+  }*/
+</script>
 <div class="col-md-8">
     <div class="qtbox">
         <h3>My <b>Enquiry Details</b></h3>
@@ -21,46 +49,30 @@
                 <li class="back_right"><a href="#"><i class="fa fa-long-arrow-left" aria-hidden="true"></i></a></li>
             </div>
 
-            <script>
-                 function showhide()
-                 {
-                 var div = document.getElementById("newpost");
-                    if (div.style.display !== "block") {
-                        div.style.display = "block";
-                        document.getElementById("abc").style.backgroundColor = "#fff";
-                    }
-                    else {
-                        div.style.display = "none";
-                        document.getElementById("abc").style.backgroundColor = "#f2f2f2";
-                    }
-                     }
-
-                     function showhide1()
-                 {
-                 var div = document.getElementById("newpost1");
-                    if (div.style.display !== "block") {
-                        div.style.display = "block";
-                        document.getElementById("abc1").style.backgroundColor = "#fff";
-                    }
-                    else {
-                        div.style.display = "none";
-                        document.getElementById("abc1").style.backgroundColor = "#f2f2f2";
-                    }
-                     }
-          </script>
+            
 
              <div class="mailarea">
-                <div id="abc" class="loopbox">
-                    <a href="javascript:void(0)" onclick="showhide()">
+                @if (count($patient_enq_data) > 0)
+                    @foreach($patient_enq_data as $key=>$val)
+                <div id="abc<?php echo $val['enq_detail_id']; ?>" class="loopbox">
+                    <a href="javascript:void(0)" onclick="showhide(<?php echo $val['enq_detail_id']; ?>)" >
                       <div class="row">
                           <div class="col-xs-1">
-                            <img src="{!!URL::to('storage/frontend/images/master.jpg')!!}"  alt="Image 1">
+                            @if(!empty($val['sender_image']))
+                              @php
+                                $sender_image=$val['sender_image'];
+                              @endphp
+                             <img src="{!!URL::to('/uploads/patients/thumb/'.$sender_image)!!}"  alt="Image 1">
+                            @else 
+                             <img src="{!!URL::to('storage/frontend/images/noimage.jpg')!!}"  alt="Image 1">
+                            @endif
                           </div>
-                          <div class="col-xs-9"><div class="loopname">Master Mondal</div></div>
-                          <div class="col-xs-2" align="right"><div class="loopname">7/12/2016</div></div>
+                          <div class="col-xs-9"><div class="loopname">{{ $val['sender_name'] }}</div></div>
+                          <div class="col-xs-2" align="right"><div class="loopname">{{ date("d F Y",strtotime($val['created_at'])) }} </div></div>
                       </div>
                     </a>
-                    <div id="newpost" class="maildet" style="display:none;">
+
+                    <div id="newpost<?php echo $val['enq_detail_id']; ?>" style="display:none;">
                       <div class="mailsender">
                       to me
                       <li class="dropdown">
@@ -79,12 +91,13 @@
                         </ul>
                       </li>
                       <p>
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.
+                     {!! $val['message'] !!} 
                       </p>
                     </div>
                 </div>
-
-                <div id="abc1" class="loopbox">
+                 @endforeach
+              @endif
+                <!-- <div id="abc1" class="loopbox">
                     <a href="javascript:void(0)" onclick="showhide1()">
                       <div class="row">
                           <div class="col-xs-1">
@@ -116,7 +129,7 @@
                       Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.
                       </p>
                     </div>
-                </div>
+                </div> -->
 
 
              </div>
